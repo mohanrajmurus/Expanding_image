2eb1d37b95abd94bfd659b6ac531a2b2e8b99f07
Mohanraj <mohanrajmurus@gmail.com>
2022-07-01 14:43:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const panels = document.querySelectorAll(".panel");
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click',() => {
+        removeClass();
+        panel.classList.add('active');
+    })
+})
+
+
+
+
+
+function removeClass(){
+    panels.forEach(panel => {
+        panel.classList.remove("active");
+    })
+}
